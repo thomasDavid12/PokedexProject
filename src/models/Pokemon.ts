0d830b24579ae57a52d7fac42e2c8c30135101e7
Thomas D <you@example.com>
2023-12-16 20:27:22 +0100

    Add new files and configurations

---
+++ src/models/Pokemon.ts
@@ -0,0 +1,9 @@
+export class Pokemon {
+    name: string;
+    pokedexNumber: number;
+
+    constructor(name: string, pokedexNumber: number) {
+        this.name = name;
+        this.pokedexNumber = pokedexNumber;
+    }
+}
