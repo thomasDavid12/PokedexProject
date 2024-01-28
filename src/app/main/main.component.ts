299513f8b0602da070c85a37d2daf640ce4fe75c
Thomas D <th.david12@gmail.com>
2024-01-28 12:25:20 +0100

    Ajout d'un commentaire sur updateList

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -35,9 +35,21 @@ export class MainComponent {
     });
   }
 
+  // Fonction : updateList
+  // Paramètres :
+  //   - $event : tout type (any) - représente l'événement déclencheur de la mise à jour (probablement un événement d'entrée, comme une saisie utilisateur)
+  // Description :
+  //   Cette fonction est utilisée pour mettre à jour la liste des Pokémon filtrés en fonction de la valeur entrée par l'utilisateur.
+  //   Elle prend en compte la valeur de la propriété 'filter' qui est mise à jour avec la valeur de l'événement $event.
+  //   Si la longueur de la chaîne de filtre atteint ou dépasse 3 caractères, la liste des Pokémon filtrés est mise à jour en fonction du filtre.
+  //   Sinon, la liste des Pokémon filtrés est réinitialisée pour afficher tous les Pokémon disponibles.
+  // Remarques :
+  //   - La comparaison des noms de Pokémon se fait en insensible à la casse pour assurer une correspondance appropriée.
+  //   - La fonction utilise des variables locales telles que 'pokemonMinuscule' et 'filterMinuscule' pour effectuer la comparaison.
+  //   - Il est probable que cette fonction soit liée à une fonctionnalité de filtrage dans une application Pokemon.
   updateList($event:any) {
     this.filter = $event.target.value
-    
+
     if(this.filter.length >= 3) {
       this.filteredPokemons = this.pokemons.filter(p => {
         const pokemonMinuscule = p.name.toLowerCase();
